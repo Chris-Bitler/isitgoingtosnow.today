229e20498d9b938e82cc94c9ffe50c713b67a69e
Chris Bitler <crb2547@rit.edu>
2017-10-31 13:17:10 -0400

    Update rochester file

--- rochester.php
+++ rochester.php
@@ -1,6 +1,14 @@
-<!-- This file is a joke. It is never not snowing in Rochester, New York -->
+<!-- This file is a joke. It is never not snowing in Rochester, New York, except for when it's under construction -->
 <html>
     <body>
-        <h1 style="font-size:100px; margin-top:250px; text-align: center;">YES</h1>
+    <?php
+        $month = (int) date('n');
+        if($month >= 11 || $month <= 5) {
+            echo '<h1 style="font-size:100px; margin-top:250px; text-align: center;">YES</h1>';
+        } else {
+            echo '<h1 style="font-size:50px; margin-top:250px; text-align: center;">No, It\'s construction season</h1>';
+        }
+    ?>
+
     </body>
 </html>
