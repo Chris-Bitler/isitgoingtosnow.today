25e6efebddbe8f9086ef9026e64b1225cd95969a
Chris Bitler <crb2547@rit.edu>
2017-10-31 13:11:36 -0400

    Add rochester file

---
+++ rochester.php
@@ -0,0 +1,6 @@
+<!-- This file is a joke. It is never not snowing in Rochester, New York -->
+<html>
+    <body>
+        <h1 style="font-size:100px; margin-top:250px; text-align: center;">YES</h1>
+    </body>
+</html>
